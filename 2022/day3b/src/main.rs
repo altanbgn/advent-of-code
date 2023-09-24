1d12e349b2f1f17060ad1d40162458b6d3b08c8d
altanbgn <altanbagana@protonmail.com>
2023-09-24 20:17:59 +0800

    pog

--- 2022/day3b/src/main.rs
+++ 2022/day3b/src/main.rs
@@ -1,35 +1,56 @@
 fn main() {
     let mut sum = 0;
 
-    let _ = include_str!("../input.txt")
-        .lines()
-        .for_each(|rucksack| {
-            let mut duplicate_char = None;
-            let rucksack_split = rucksack.split_at(rucksack.len() / 2);
+    let mut count = 0;
+    let mut first_rucksack: Option<String> = None;
+    let mut second_rucksack: Option<String> = None;
 
-            rucksack_split.0.chars().for_each(|char_1| {
-                rucksack_split.1.chars().for_each(|char_2| {
-                    if char_1 == char_2 {
-                        duplicate_char = Some(char_1);
-                    }
-                });
+    include_str!("../input.txt").lines().for_each(|item| {
+        if count == 0 {
+            count += 1;
+            first_rucksack = Some(item.to_string());
+            return;
+        }
+        if count == 1 {
+            count += 1;
+            second_rucksack = Some(item.to_string());
+            return;
+        }
+        if count == 2 {
+            // Comparing starts!
+            let mut key: Option<char> = None;
+
+            count = 0;
+
+            first_rucksack.as_ref().unwrap().chars().for_each(|item_1| {
+                second_rucksack
+                    .as_ref()
+                    .unwrap()
+                    .chars()
+                    .for_each(|item_2| {
+                        item.chars().for_each(|item_3| {
+                            if item_1 == item_2 && item_2 == item_3 {
+                                key = Some(item_1);
+                            }
+                        })
+                    });
             });
 
-            let _ = match duplicate_char {
+            match key {
                 Some(_) => {
-                    let duplicate_char_as_num = duplicate_char.unwrap() as u32;
+                    let key_as_num = key.unwrap() as u32;
 
-                    if duplicate_char_as_num >= 64 && duplicate_char_as_num < 97 {
-                        return sum += duplicate_char.unwrap() as u32 - 38;
+                    if key_as_num >= 64 && key_as_num < 97 {
+                        return sum += key.unwrap() as u32 - 38;
                     }
-                    if duplicate_char_as_num >= 97 {
-                        return sum += duplicate_char.unwrap() as u32 - 96;
+                    if key_as_num >= 97 {
+                        return sum += key.unwrap() as u32 - 96;
                     }
-                },
-                None => ()
-            };
-
-        });
+                }
+                None => (),
+            }
+        }
+    });
 
     println!("{}", sum);
 }
---
+++ 2022/day4a/src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    let mut sum = 0;
+
+    include_str!("../input.txt").lines().for_each(|item| {
+        let pairs: Vec<&str> = item.split(",").collect();
+
+        let first_section: Vec<i32> = pairs[0]
+            .split("-")
+            .map(|item| item.parse::<i32>().unwrap())
+            .collect();
+
+        let second_section: Vec<i32> = pairs[1]
+            .split("-")
+            .map(|item| item.parse::<i32>().unwrap())
+            .collect();
+
+        if (first_section[0] <= second_section[0] && first_section[1] >= second_section[1])
+            || (first_section[0] >= second_section[0] && first_section[1] <= second_section[1])
+        {
+            sum += 1;
+        }
+    });
+
+    println!("{}", sum);
+}
