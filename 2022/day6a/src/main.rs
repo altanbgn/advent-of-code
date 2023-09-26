eb6bc2eef1228d433368be0b5090fd50e9b5cb58
altanbgn <altanbagana.e@gobicashmere.com>
2023-09-26 15:39:58 +0800

    day6a solved

--- 2022/day6a/src/main.rs
+++ 2022/day6a/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    let buffer: Vec<char> = include_str!("../input.txt").trim().chars().collect();
+    let mut num: usize = 0;
+
+    buffer.iter().enumerate().for_each(|(index, character)| {
+        if num != 0 {
+            return;
+        }
+
+        if index + 3 >= buffer.len() {
+            return;
+        }
+
+        if character != &buffer[index + 1]
+            && character != &buffer[index + 2]
+            && character != &buffer[index + 3]
+            && &buffer[index + 1] != &buffer[index + 2]
+            && &buffer[index + 1] != &buffer[index + 3]
+            && &buffer[index + 2] != &buffer[index + 3]
+        {
+            println!("{}, {}, {}, {}", character, &buffer[index + 1], &buffer[index + 2], &buffer[index + 3]);
+            num = index + 4;
+            return;
+        }
+    });
+
+    println!("{}", num);
 }
